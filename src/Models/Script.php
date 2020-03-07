a789ad9be63599fd7b5637851a3564fbd4cbe0a0
D. van der Heiden <info@vdhicts.nl>
2020-03-07 20:09:46 +0100

    Finish Rebrandly implementation

--- src/Models/Script.php
+++ src/Models/Script.php
@@ -2,29 +2,117 @@
 
 namespace Vdhicts\Rebrandly\Models;
 
-class Script
+use stdClass;
+use Vdhicts\Rebrandly\Contracts\Model;
+
+class Script implements Model
 {
     /**
      * Unique identifier of script.
      * @var string
      */
-    private $id;
+    private $id = '';
 
     /**
      * Unique name of the script.
      * @var string
      */
-    private $name;
+    private $name = '';
 
     /**
      * Javascript snippet (enclosed into <script> and </script> HTML tags)
      * @var string
      */
-    private $value;
+    private $value = '';
 
     /**
      * Publicly accessible URL to the script content
      * @var string
      */
-    private $uri;
+    private $uri = '';
+
+    /**
+     * @param stdClass $response
+     * @return Script
+     */
+    public static function fromResponse(stdClass $response): Script
+    {
+        return (new self())
+            ->setId($response->id)
+            ->setName($response->name)
+            ->setUri($response->uri)
+            ->setValue($response->value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Script
+     */
+    public function setId(string $id): Script
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Script
+     */
+    public function setName(string $name): Script
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     * @return Script
+     */
+    public function setValue(string $value): Script
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param string $uri
+     * @return Script
+     */
+    public function setUri(string $uri): Script
+    {
+        $this->uri = $uri;
+        return $this;
+    }
 }
--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -2,23 +2,92 @@
 
 namespace Vdhicts\Rebrandly\Models;
 
-class Tag
+use stdClass;
+use Vdhicts\Rebrandly\Contracts\Model;
+
+class Tag implements Model
 {
     /**
      * Unique identifier of a tag.
      * @var string
      */
-    private $id;
+    private $id = '';
 
     /**
      * Unique name of a tag.
      * @var string
      */
-    private $name;
+    private $name = '';
 
     /**
      * Hexadecimal representation of a color assigned to a tag.
      * @var string
      */
-    private $color;
+    private $color = '';
+
+    /**
+     * @param stdClass $response
+     * @return Tag
+     */
+    public static function fromResponse(stdClass $response): Tag
+    {
+        return (new self())
+            ->setId($response->id)
+            ->setName($response->name)
+            ->setColor($response->color);
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Tag
+     */
+    public function setId(string $id): Tag
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Tag
+     */
+    public function setName(string $name): Tag
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     * @return Tag
+     */
+    public function setColor(string $color): Tag
+    {
+        $this->color = $color;
+        return $this;
+    }
 }
